c3b9e799fd7321b391342b638641541deaebba2c
Scott Alzheimer <salzheimer@users.noreply.github.com>
2026-05-21 15:12:08 -0700

    redudent authenticate user check removed on permission controller

--- services/IdentityService/src/Controllers/PermissionController.cs
+++ services/IdentityService/src/Controllers/PermissionController.cs
@@ -22,22 +22,12 @@ public class PermissionController : BaseController
     [HttpPost]
     public async Task<IActionResult> GrantPermission([FromBody] ResourcePermissionCreateDto permission)
     {
-        var authenticatedUserId = GetAuthenticatedUserId();
-        if (authenticatedUserId == null)
-        {
-            return HandleResult(Result<ResourcePermissionDto>.Failure(PermissionErrors.Unauthorized));
-        }
-        var result = HandleResult(await _permissionService.AddPermissionAsync(permission));
-        return result;
+        return HandleResult(await _permissionService.AddPermissionAsync(permission));
     }
+
     [HttpGet("resource-permissions")]
     public async Task<IActionResult> GetResourcePermissions(string resourceType, int resourceId)
     {
-        var authenticatedUserId = GetAuthenticatedUserId();
-        if (authenticatedUserId == null)
-        {
-            return HandleResult(Result<IEnumerable<ResourcePermissionDto>>.Failure(PermissionErrors.Unauthorized));
-        }
         Shared.Models.ResourceType sharedResourceType;
         if (Enum.TryParse<Shared.Models.ResourceType>(resourceType, true, out var parsedResourceType))
         {
@@ -48,17 +38,12 @@ public class PermissionController : BaseController
             return HandleResult(Result<IEnumerable<ResourcePermissionDto>>.Failure(PermissionErrors.InvalidResourceType));
         }
 
-        var permissions = HandleResult(await _permissionService.GetPermissionsForResourceAsync(sharedResourceType, resourceId));
-        return permissions;
+        return HandleResult(await _permissionService.GetPermissionsForResourceAsync(sharedResourceType, resourceId));
     }
+
     [HttpGet("subject-permissions")]
     public async Task<IActionResult> GetSubjectPermissions(string subjectType, int subjectId)
     {
-        var authenticatedUserId = GetAuthenticatedUserId();
-        if (authenticatedUserId == null)
-        {
-            return HandleResult(Result<IEnumerable<ResourcePermissionDto>>.Failure(PermissionErrors.Unauthorized));
-        }
         Shared.Models.SubjectType sharedSubjectType;
         if (Enum.TryParse<Shared.Models.SubjectType>(subjectType, true, out var parsedSubjectType))
         {
@@ -68,22 +53,8 @@ public class PermissionController : BaseController
         {
             return HandleResult(Result<IEnumerable<ResourcePermissionDto>>.Failure(PermissionErrors.InvalidSubjectType));
         }
-        var permissions = HandleResult(await _permissionService.GetPermissionsForSubjectAsync(sharedSubjectType, subjectId));
-        return permissions;
+        return HandleResult(await _permissionService.GetPermissionsForSubjectAsync(sharedSubjectType, subjectId));
     }
-    // [HttpGet("user-permissions")]
-    // [Authorize]
-    // public async Task<IActionResult> GetUserPermissions(int userId)
-    // {
-    //     var authenticatedUserId = GetAuthenticatedUserId();
-    //     if (authenticatedUserId == null)
-    //     {
-    //         return HandleResult(Result<IEnumerable<ResourcePermissionDto>>.Failure(UserErrors.Unauthorized));
-    //     }
-
-    //     var permissions = HandleResult(await _permissionService.GetPermissionsForSubjectAsync(Shared.Models.SubjectType.User, userId));
-    //     return permissions;
-    // }
 
     [HttpDelete("{permissionId:long}")]
     public async Task<IActionResult> RevokePermission(long permissionId)
